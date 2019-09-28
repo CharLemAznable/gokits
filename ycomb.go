3ea24bc2c781a4e7e40a73f6d7537e750b9a26f7
CharLemAznable <545541819@qq.com>
2019-09-29 01:50:01 +0800

    Some refactor.

--- ycomb.go
+++ ycomb.go
@@ -3,14 +3,11 @@ package gokits
 type RecFunc func(n interface{}) interface{}
 
 func YComb(b func(RecFunc) RecFunc) RecFunc {
-    var g = func(f func(t RecFunc) RecFunc) RecFunc {
-        var r = func(y interface{}) RecFunc {
-            var w = y.(func(v interface{}) RecFunc)
-            return f(func(n interface{}) interface{} {
-                return w(w)(n)
-            })
-        }
-        return r(r)
+    var r = func(y interface{}) RecFunc {
+        var w = y.(func(v interface{}) RecFunc)
+        return b(func(n interface{}) interface{} {
+            return w(w)(n)
+        })
     }
-    return g(b)
+    return r(r)
 }
