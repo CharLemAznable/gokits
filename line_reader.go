d647adc6a61671ad9a34df6d3a2669abd9d538e2
CharLemAznable <545541819@qq.com>
2019-10-25 11:04:34 +0800

    Try fixed some sonar code smell.

--- line_reader.go
+++ line_reader.go
@@ -34,97 +34,33 @@ func (lineReader *LineReader) ReadLine() (int, error) {
     skipLF := false
 
     for true {
-        if lineReader.inOff >= lineReader.inLimit {
-            n, err := lineReader.reader.Read(lineReader.inByteBuf)
-            lineReader.inLimit = n
-            lineReader.inOff = 0
-            if nil != err || lineReader.inLimit <= 0 {
-                if length == 0 || isCommentLine {
-                    return -1, err
-                }
-                if precedingBackslash {
-                    length--
-                }
-                return length, nil
-            }
+        ret, lth, err := lineReader.readInByteBuf(
+            length, isCommentLine, precedingBackslash)
+        if ret {
+            return lth, err
+        } else {
+            length = lth
         }
 
         c = lineReader.inByteBuf[lineReader.inOff]
         lineReader.inOff++
 
-        if skipLF {
-            skipLF = false
-            if c == '\n' {
-                continue
-            }
-        }
-        if skipWhiteSpace {
-            if c == ' ' || c == '\t' || c == '\f' {
-                continue
-            }
-            if !appendedLineBegin && (c == '\r' || c == '\n') {
-                continue
-            }
-            skipWhiteSpace = false
-            appendedLineBegin = false
-        }
-        if isNewLine {
-            isNewLine = false
-            if c == '#' || c == '!' {
-                isCommentLine = true
-                continue
-            }
+        if lineReader.readSkip(&skipLF, &c,
+            &skipWhiteSpace, &appendedLineBegin,
+            &isNewLine, &isCommentLine) {
+            continue
         }
 
         if c != '\n' && c != '\r' {
-            lineReader.lineBuf[length] = c
-            length++
-            if length == len(lineReader.lineBuf) {
-                newLength := length * 2
-                if newLength < 0 {
-                    newLength = int(^uint(0) >> 1)
-                }
-                buf := make([]byte, newLength)
-                copy(buf, lineReader.lineBuf)
-                lineReader.lineBuf = buf
-            }
-            // flip the preceding backslash flag
-            if c == '\\' {
-                precedingBackslash = !precedingBackslash
-            } else {
-                precedingBackslash = false
-            }
+            lineReader.readLine(&length, c, &precedingBackslash)
         } else {
-            // reached EOL
-            if isCommentLine || length == 0 {
-                isCommentLine = false
-                isNewLine = true
-                skipWhiteSpace = true
-                length = 0
-                continue
-            }
-            if lineReader.inOff >= lineReader.inLimit {
-                n, err := lineReader.reader.Read(lineReader.inByteBuf)
-                lineReader.inLimit = n
-                lineReader.inOff = 0
-                if nil != err || lineReader.inLimit <= 0 {
-                    if precedingBackslash {
-                        length--
-                    }
-                    return length, err
-                }
-            }
-            if precedingBackslash {
-                length -= 1
-                // skip the leading whitespace characters in following line
-                skipWhiteSpace = true
-                appendedLineBegin = true
-                precedingBackslash = false
-                if c == '\r' {
-                    skipLF = true
-                }
+            ret, lth, err := lineReader.readEOL(&isCommentLine, &length,
+                &isNewLine, &skipWhiteSpace, &precedingBackslash,
+                &appendedLineBegin, c, &skipLF)
+            if ret {
+                return lth, err
             } else {
-                return length, nil
+                length = lth
             }
         }
     }
@@ -132,6 +68,112 @@ func (lineReader *LineReader) ReadLine() (int, error) {
     return -1, &LineReaderException{
         Message: "Unexpected Error",
     }
+}
+
+func (lineReader *LineReader) readInByteBuf(length int, isCommentLine bool, precedingBackslash bool) (bool, int, error) {
+    if lineReader.inOff >= lineReader.inLimit {
+        n, err := lineReader.reader.Read(lineReader.inByteBuf)
+        lineReader.inLimit = n
+        lineReader.inOff = 0
+        if nil != err || lineReader.inLimit <= 0 {
+            if length == 0 || isCommentLine {
+                return true, -1, err
+            }
+            if precedingBackslash {
+                length--
+            }
+            return true, length, nil
+        }
+    }
+    return false, length, nil
+}
+
+func (lineReader *LineReader) readSkip(skipLF *bool, c *byte,
+    skipWhiteSpace *bool, appendedLineBegin *bool,
+    isNewLine *bool, isCommentLine *bool) bool {
+
+    if *skipLF {
+        *skipLF = false
+        if *c == '\n' {
+            return true
+        }
+    }
+    if *skipWhiteSpace {
+        if *c == ' ' || *c == '\t' || *c == '\f' {
+            return true
+        }
+        if !*appendedLineBegin && (*c == '\r' || *c == '\n') {
+            return true
+        }
+        *skipWhiteSpace = false
+        *appendedLineBegin = false
+    }
+    if *isNewLine {
+        *isNewLine = false
+        if *c == '#' || *c == '!' {
+            *isCommentLine = true
+            return true
+        }
+    }
+    return false
+}
+
+func (lineReader *LineReader) readLine(length *int, c byte, precedingBackslash *bool) {
+    lineReader.lineBuf[*length] = c
+    *length++
+    if *length == len(lineReader.lineBuf) {
+        newLength := *length * 2
+        if newLength < 0 {
+            newLength = int(^uint(0) >> 1)
+        }
+        buf := make([]byte, newLength)
+        copy(buf, lineReader.lineBuf)
+        lineReader.lineBuf = buf
+    }
+    // flip the preceding backslash flag
+    if c == '\\' {
+        *precedingBackslash = !*precedingBackslash
+    } else {
+        *precedingBackslash = false
+    }
+}
+
+func (lineReader *LineReader) readEOL(isCommentLine *bool, length *int,
+    isNewLine *bool, skipWhiteSpace *bool, precedingBackslash *bool,
+    appendedLineBegin *bool, c byte, skipLF *bool) (bool, int, error) {
+
+    // reached EOL
+    if *isCommentLine || *length == 0 {
+        *isCommentLine = false
+        *isNewLine = true
+        *skipWhiteSpace = true
+        *length = 0
+        return false, *length, nil
+    }
+    if lineReader.inOff >= lineReader.inLimit {
+        n, err := lineReader.reader.Read(lineReader.inByteBuf)
+        lineReader.inLimit = n
+        lineReader.inOff = 0
+        if nil != err || lineReader.inLimit <= 0 {
+            if *precedingBackslash {
+                *length--
+            }
+            return true, *length, err
+        }
+    }
+    if *precedingBackslash {
+        *length -= 1
+        // skip the leading whitespace characters in following line
+        *skipWhiteSpace = true
+        *appendedLineBegin = true
+        *precedingBackslash = false
+        if c == '\r' {
+            *skipLF = true
+        }
+    } else {
+        return true, *length, nil
+    }
+    return false, *length, nil
 }
 
 type LineReaderException struct {
