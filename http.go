f85c7afcd15661b9cfad85b86636cdac8be6d820
CharLemAznable <545541819@qq.com>
2020-01-08 01:34:25 +0800

    Add FormIntValue().

--- http.go
+++ http.go
@@ -1,48 +1,54 @@
 package gokits
 
 import (
-    "net/http"
+	"net/http"
 )
 
-//noinspection GoUnusedExportedFunction
+func FormIntValue(request *http.Request, key string) int {
+	return FormIntValueDefault(request, key, 0)
+}
+
+func FormIntValueDefault(request *http.Request, key string, defaultValue int) int {
+	formValue := request.FormValue(key)
+	if intValue, err := IntFromStr(formValue); nil == err {
+		return intValue
+	}
+	return defaultValue
+}
+
 func ResponseJson(writer http.ResponseWriter, json string) {
-    ResponseContent(writer, json, "application/json", "UTF-8")
+	ResponseContent(writer, json, "application/json", "UTF-8")
 }
 
-//noinspection GoUnusedExportedFunction
 func ResponseText(writer http.ResponseWriter, text string) {
-    ResponseContent(writer, text, "text/plain", "UTF-8")
+	ResponseContent(writer, text, "text/plain", "UTF-8")
 }
 
-//noinspection GoUnusedExportedFunction
 func ResponseHtml(writer http.ResponseWriter, html string) {
-    ResponseContent(writer, html, "text/html", "UTF-8")
+	ResponseContent(writer, html, "text/html", "UTF-8")
 }
 
 func ResponseContent(writer http.ResponseWriter,
-    content, contentType, characterEncoding string) {
-    writer.Header().Set("Content-Type",
-        contentType+"; charset="+characterEncoding)
-    _, _ = writer.Write([]byte(content))
+	content, contentType, characterEncoding string) {
+	writer.Header().Set("Content-Type",
+		contentType+"; charset="+characterEncoding)
+	_, _ = writer.Write([]byte(content))
 }
 
-//noinspection GoUnusedExportedFunction
 func ResponseErrorJson(writer http.ResponseWriter, statusCode int, json string) {
-    ResponseErrorContent(writer, statusCode, json, "application/json", "UTF-8")
+	ResponseErrorContent(writer, statusCode, json, "application/json", "UTF-8")
 }
 
-//noinspection GoUnusedExportedFunction
 func ResponseErrorText(writer http.ResponseWriter, statusCode int, text string) {
-    ResponseErrorContent(writer, statusCode, text, "text/plain", "UTF-8")
+	ResponseErrorContent(writer, statusCode, text, "text/plain", "UTF-8")
 }
 
-//noinspection GoUnusedExportedFunction
 func ResponseErrorHtml(writer http.ResponseWriter, statusCode int, html string) {
-    ResponseErrorContent(writer, statusCode, html, "text/html", "UTF-8")
+	ResponseErrorContent(writer, statusCode, html, "text/html", "UTF-8")
 }
 
 func ResponseErrorContent(writer http.ResponseWriter, statusCode int,
-    errorContent, contentType, characterEncoding string) {
-    writer.WriteHeader(statusCode)
-    ResponseContent(writer, errorContent, contentType, characterEncoding)
+	errorContent, contentType, characterEncoding string) {
+	writer.WriteHeader(statusCode)
+	ResponseContent(writer, errorContent, contentType, characterEncoding)
 }
